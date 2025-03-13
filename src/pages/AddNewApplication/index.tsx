d6c1b18221d8cf8b320018c44e5ab2428992f0b3
Aurore Agneray <aurore.agn@outlook.fr>
2025-03-13 21:25:35 +0100

    Mise à jour styles de la page AddNewApplication (positionnement bouton, marges, largeur champ date)

--- src/pages/AddNewApplication/index.tsx
+++ src/pages/AddNewApplication/index.tsx
@@ -8,76 +8,88 @@ import InputField from "../../components/InputField";
 
 export default function AddNewApplication() {
     return (
-        <Container>
-            {/* <Form
+        <>
+            <style>
+                {`
+                .new-application-btn-col {
+                    text-align: right;
+                }
+            `}
+            </style>
+            <Container>
+                {/* <Form
             method="post"
             action="/api/add-application"
         > */}
-            <h2>Ajout d'une nouvelle candidature</h2>
-            <Row>
-                <Col>
-                    <InputField
-                        ControlId="formDate"
-                        Label="Date de candidature"
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <InputField
-                        ControlId="formSource"
-                        Label="Source"
-                    />
-                </Col>
-                <Col>
-                    <InputField
-                        ControlId="formOfferUrl"
-                        Label="Url de l'offre"
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <InputField
-                        ControlId="formPositionName"
-                        Label="Intitulé du poste"
-                    />
-                </Col>
-                <Col>
-                    <InputField
-                        ControlId="formPlace"
-                        Label="Lieu"
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <InputField
-                        ControlId="formMotivations"
-                        Label="Mes motivations"
-                        Type="textarea"
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <InputField
-                        ControlId="formNotes"
-                        Label="Mes notes"
-                        Type="textarea"
-                    />
-                </Col>
-            </Row>
+                <h2>Ajout d'une nouvelle candidature</h2>
+                <Row>
+                    <Col xs={6}>
+                        <InputField
+                            ControlId="formDate"
+                            Label="Date de candidature"
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <InputField
+                            ControlId="formSource"
+                            Label="Source"
+                        />
+                    </Col>
+                    <Col>
+                        <InputField
+                            ControlId="formOfferUrl"
+                            Label="Url de l'offre"
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <InputField
+                            ControlId="formPositionName"
+                            Label="Intitulé du poste"
+                        />
+                    </Col>
+                    <Col>
+                        <InputField
+                            ControlId="formPlace"
+                            Label="Lieu"
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <InputField
+                            ControlId="formMotivations"
+                            Label="Mes motivations"
+                            Type="textarea"
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <InputField
+                            ControlId="formNotes"
+                            Label="Mes notes"
+                            Type="textarea"
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="new-application-btn-col">
+                        <Button
+                            variant="primary"
+                            type="submit"
+                            size="lg"
+                        >
+                            Valider
+                        </Button>
+                    </Col>
+                </Row>
 
-            <Button
-                variant="primary"
-                type="submit"
-                size="lg"
-            >
-                Valider
-            </Button>
-
-            {/* </Form> */}
-        </Container>
+                {/* </Form> */}
+            </Container>
+        </>
     );
 }

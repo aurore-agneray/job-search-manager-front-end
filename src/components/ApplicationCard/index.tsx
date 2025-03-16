e8b4743a863852cf02cb600aac382bb8d81627b2
Aurore Agneray <aurore.agn@outlook.fr>
2025-03-16 23:37:14 +0100

    Complétion de la page DisplayApplication : - Création de nombreux composants utilisés en commun avec le composant ApplicationCard afin d'afficher chacune des informations de la candidature => allègement du contenu d'ApplicationCard et ajout des fichiers ApplicationParts et FeelingIcons - Suppression de styles du fichier styles/index.css

--- src/components/ApplicationCard/index.tsx
+++ src/components/ApplicationCard/index.tsx
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 import { useNavigate } from "react-router";
 import { ApplicationType } from "../../types";
-import Icon from "@mdi/react";
-import { mdiMapMarkerOutline, mdiRobotLove, mdiHeartBroken } from "@mdi/js";
+import {
+    ApplicationDate,
+    LocationName,
+    Position,
+    Contacts,
+    OfferUrl
+} from "../ApplicationParts";
 import StatusIcon from "../StatusIcon";
+import FeelingIcons from "../FeelingIcons";
 
 type SubPartProps = {
     $widthPercentage?: number;
@@ -49,6 +55,11 @@ const SubPart = styled.div<SubPartProps>`
     overflow-wrap: anywhere;
 `;
 
+const JobTitleSubPart = styled(SubPart)`
+    border-right: 3px dotted #ec994c;
+    padding-right: 20px;
+`;
+
 const DateDiv = styled.div`
     position: absolute;
     right: 0px;
@@ -56,34 +67,6 @@ const DateDiv = styled.div`
     font-weight: 500;
     color: var(--bs-primary);
 `;
-
-const getPositionFunc = (isSpontaneous: boolean, position: string) => {
-    if (isSpontaneous) {
-        return "Candidature spontanée";
-    }
-    return position;
-};
-
-const getFeelingIconsFunc = (applicationId: string, feelingLevel: number) => {
-    if (feelingLevel === 0) {
-        return (
-            <Icon
-                color="var(--my-var-bad-feeling-color)"
-                path={mdiHeartBroken}
-                size={1}
-            />
-        );
-    }
-
-    return [1, 2, 3, 4, 5].slice(0, feelingLevel).map((lvl) => (
-        <Icon
-            color="var(--my-var-good-feeling-color)"
-            key={applicationId + lvl}
-            path={mdiRobotLove}
-            size={1}
-        />
-    ));
-};
 
 export default function ApplicationCard(props: ApplicationType) {
     const navigate = useNavigate();
@@ -95,53 +78,38 @@ export default function ApplicationCard(props: ApplicationType) {
             <MainPart>
                 {props.Date && (
                     <DateDiv>
-                        Ai{" "}
-                        {props.IsFromMyInitiative ? " postulé " : " répondu "}
-                        le {" " + props.Date.toLocaleString()}
+                        <ApplicationDate
+                            IsFromMyInitiative={props.IsFromMyInitiative}
+                            Date={props.Date}
+                            MarginRight={0}
+                        />
                     </DateDiv>
                 )}
-                <SubPart className="job-title-subpart">
+                <JobTitleSubPart>
                     <h3>{props.Source}</h3>
                     <h4>
-                        {getPositionFunc(props.IsSpontaneous, props.Position)}
+                        <Position
+                            IsSpontaneous={props.IsSpontaneous}
+                            Position={props.Position}
+                        />
                     </h4>
-                </SubPart>
+                </JobTitleSubPart>
                 <SubPart
                     $marginTop={50}
                     $widthPercentage={70}
                     $justifyContent="space-between"
                 >
-                    <p className="location-name">
-                        <Icon
-                            path={mdiMapMarkerOutline}
-                            size={1}
-                        />{" "}
-                        {props.Place}
-                    </p>
-                    {props.Contacts && (
-                        <p>
-                            <span className="underlined-text">Contacts</span>{" "}
-                            <br />
-                            {props.Contacts}
-                        </p>
-                    )}
-                    <p className="feeling-icons">
-                        {getFeelingIconsFunc(props.Id, props.FeelingLevel)}
-                    </p>
+                    <LocationName Place={props.Place} />
+                    <Contacts Contacts={props.Contacts} />
+                    <FeelingIcons
+                        Id={props.Id}
+                        FeelingLevel={props.FeelingLevel}
+                        HorizontalAlignment="center"
+                    />
                 </SubPart>
             </MainPart>
             <FooterPart>
-                <div>
-                    {props.OfferUrl && (
-                        <a
-                            onClick={(e) => e.stopPropagation()}
-                            href={props.OfferUrl}
-                            target="_blank"
-                        >
-                            Voir l'offre
-                        </a>
-                    )}
-                </div>
+                <OfferUrl Url={props.OfferUrl} />
                 <StatusIcon status={props.Status} />
             </FooterPart>
         </MyApplicationCard>
--- src/pages/DisplayApplication/index.tsx
+++ src/pages/DisplayApplication/index.tsx
@@ -1,37 +1,130 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 import { getApplicationById } from "../../services/applications-services";
 import { Col, Container, Row } from "react-bootstrap";
+import {
+    ApplicationDate,
+    Contacts,
+    LocationName,
+    Motivations,
+    Notes,
+    OfferUrl,
+    Position
+} from "../../components/ApplicationParts";
+import StatusIcon from "../../components/StatusIcon";
+import FeelingIcons from "../../components/FeelingIcons";
+
+const AppDetailsRow = styled(Row)`
+    padding: 0.8rem 0rem;
+`;
+
+const FirstRow = styled(AppDetailsRow)`
+    padding: 1.5rem 0rem !important;
+    border-bottom: 3px dotted var(--my-var-h4-color);
+
+    & > div.col-md-12 {
+        display: flex;
+        align-items: flex-end;
+    }
+
+    & > div.col-md-12 > h1,
+    & > div.col-md-12 > h2 {
+        display: inline;
+        margin-bottom: 0px !important;
+    }
+
+    & > div.col-md-12:last-child {
+        justify-content: right;
+        padding-top: 30px !important;
+        color: var(--bs-primary);
+    }
+`;
+
+const LastRow = styled(AppDetailsRow)`
+    text-align: right;
+`;
 
 export default function DisplayApplication() {
     const { id } = useParams();
+    const appNotFoundMessage = "La candidature n'a pas été trouvée";
 
     if (!id) {
-        return <h1>Given ID not valid</h1>;
+        return <h1>{appNotFoundMessage}</h1>;
     }
 
     const application = getApplicationById(id);
 
     if (!application) {
-        return <h1>Application not found</h1>;
+        return <h1>{appNotFoundMessage}</h1>;
     }
 
     return (
         <Container>
-            <Row>
-                <Col>
-                    <h1>{application.Source}</h1>
+            <FirstRow>
+                <Col
+                    lg={8}
+                    md={12}
+                >
+                    <h2>
+                        {application.Source} {" - "}
+                        <Position
+                            IsSpontaneous={application.IsSpontaneous}
+                            Position={application.Position}
+                        />
+                    </h2>
                 </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <h2>{application.Position}</h2>
+                {application.Date && (
+                    <Col
+                        lg={4}
+                        md={12}
+                    >
+                        <ApplicationDate
+                            IsFromMyInitiative={application.IsFromMyInitiative}
+                            Date={application.Date}
+                            MarginRight={20}
+                        />
+                        <StatusIcon status={application.Status} />
+                    </Col>
+                )}
+            </FirstRow>
+            <AppDetailsRow>
+                <Col
+                    lg={8}
+                    md={12}
+                >
+                    <LocationName Place={application.Place} />
                 </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <h3>{application.Place}</h3>
+                <Col
+                    lg={4}
+                    md={12}
+                >
+                    <FeelingIcons
+                        Id={application.Id}
+                        FeelingLevel={application.FeelingLevel}
+                        HorizontalAlignment="right"
+                    />
                 </Col>
-            </Row>
+            </AppDetailsRow>
+            <AppDetailsRow>
+                <Col>
+                    <Contacts Contacts={application.Contacts} />
+                </Col>
+            </AppDetailsRow>
+            <AppDetailsRow>
+                <Col>
+                    <Motivations Motivations={application.Motivations} />
+                </Col>
+            </AppDetailsRow>
+            <AppDetailsRow>
+                <Col>
+                    <Notes Notes={application.Notes} />
+                </Col>
+            </AppDetailsRow>
+            <LastRow>
+                <Col>
+                    <OfferUrl Url={application.OfferUrl} />
+                </Col>
+            </LastRow>
         </Container>
     );
 }

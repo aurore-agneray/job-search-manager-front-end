407cfd3d3ea1804858c51684320b665eb2c16e4c
Aurore Agneray <aurore.agn@outlook.fr>
2025-04-02 21:29:36 +0200

    Ajout de styles sur le composant StatusBadge, changement d'opacité et ajout d'une ombre ou pas selon le clic effectué

--- src/components/ApplicationsFilter/StatusBadge.tsx
+++ src/components/ApplicationsFilter/StatusBadge.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 type StatusBadgeProps = {
@@ -5,18 +6,42 @@ type StatusBadgeProps = {
     bgColor: string;
 };
 
-export default function StatusBadge(props: StatusBadgeProps) {
-    const MyBadge = styled.span`
-        display: inline-block;
-        padding: 0.2em 0.8em;
-        margin: 0.35em;
-        background-color: ${props.bgColor};
-        color: white;
-        border-radius: 30px;
-        font-size: 1em;
-        text-align: center;
-        vertical-align: baseline;
-    `;
+type MyBadgeProps = {
+    $bgColor: string;
+    $selected: boolean;
+};
 
-    return <MyBadge>{props.textContent}</MyBadge>;
+const MyBadge = styled.span<MyBadgeProps>`
+    display: inline-block;
+    padding: 0.2em 0.8em;
+    margin: 0.35em;
+    background-color: ${(props) => props.$bgColor};
+    color: white;
+    border-radius: 30px;
+    font-size: 1em;
+    text-align: center;
+    vertical-align: baseline;
+    opacity: ${(props) => (props.$selected ? 0.95 : 0.4)};
+    box-shadow: ${(props) =>
+        props.$selected
+            ? "var(--my-var-shadow-status-badge) 2px 2px 2px"
+            : "none"};
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+export default function StatusBadge(props: StatusBadgeProps) {
+    const [selected, setSelected] = useState(false);
+
+    return (
+        <MyBadge
+            className="noselectable"
+            $selected={selected}
+            $bgColor={props.bgColor}
+            onClick={() => setSelected(!selected)}
+        >
+            {props.textContent}
+        </MyBadge>
+    );
 }
